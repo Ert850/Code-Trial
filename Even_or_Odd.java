1e34f50496cabe408b37172a69034d517d009778
XtremeALmond <khucker.jr@gmail.com>
2022-09-21 11:33:22 -0400

    added input method

--- Even_or_Odd.java
+++ Even_or_Odd.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class Even_or_Odd {
     public static void main(String[] args){
-
+        int numToTest;
+        Scanner input = new Scanner(System.in);
     }
 }
