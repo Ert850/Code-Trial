a6a522773f155a01d40f8d27888d6482b749f55b
XtremeALmond <khucker.jr@gmail.com>
2022-09-21 11:36:19 -0400

    Added testing logic

--- Even_or_Odd.java
+++ Even_or_Odd.java
@@ -3,5 +3,15 @@ public class Even_or_Odd {
     public static void main(String[] args){
         int numToTest;
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an integer (whole number) to find out if it is Even or Odd\n");
+        numToTest = input.nextInt();
+
+        if(numToTest % 2 == 0){
+            System.out.println(numToTest + " is a even number");
+        }
+        else{
+            System.out.println(numToTest + " is an odd number");
+        }
     }
 }
