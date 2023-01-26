aa2bfc987aa20e778cb8164e40f3499fbfe37a80
Ert850 <elijah.r.thacker@gmail.com>
2023-01-26 13:40:31 -0500

    attempt on hw

---
+++ ENED S2/HW/Stats.py
@@ -0,0 +1,18 @@
+# Activity Python: Stats
+# File: Stats.py 
+# Date:    26 January 2023
+# By:      Elijah Thacker
+# Section: 005
+# Team:    063
+# 
+# ELECTRONIC SIGNATURE
+# Elijah Thacker
+#
+# The electronic signature above indicates the script
+# submitted for evaluation is my individual work, and I
+# have a general understanding of all aspects of its
+# development and execution.
+#
+# A BRIEF DESCRIPTION OF WHAT THE SCRIPT OR FUNCTION DOES
+# Statistics Function for test values
+num = input("Input a list of numerical values: ")
