06f0c5927ddd73ae76b49680f8b59366ee80d0b0
JacobFreidline <125321629+JacobFreidline@users.noreply.github.com>
2023-02-13 21:03:39 -0500

    python

---
+++ JF/E4_P19_freidljs.py
@@ -0,0 +1,38 @@
+# Assignment
+# File:    E4_P19_freidljs
+# Date:    2/13/23
+# By:      Jacob Freidline
+# Section: 005
+# Team:    72
+# 
+# ELECTRONIC SIGNATURE
+# Jacob Freidline
+#
+# The electronic signature above indicates the script
+# submitted for evaluation is my individual work, and I
+# have a general understanding of all aspects of its
+# development and execution.
+#
+# A BRIEF DESCRIPTION OF WHAT THE SCRIPT OR FUNCTION DOES
+# This script is a header template that will be used for 
+# all your python files the rest of the semester.
+
+import math
+
+Vo = input(float('Enter the initial velocity (m/s): '))
+N = input(float('Enter the number of angles: '))
+
+for k in range(N):
+    angleD = input(float('Enter the angle (in degrees): '))
+    while angleD <= 0:
+        angleD = input(float('Enter the angle (in degrees): '))
+    while angleD >= 90:
+        angleD = input(float('Enter the angle (in degrees): '))
+    angleA = angleD*math.pi/180
+    maxH = ((Vo**2)*(math.sin(angleA)**2))/19.62
+    maxR = (2(Vo**2)*math.cos(angleA)*math.sin(angleA))/19.62
+
+print('Velocity: {0}\n'.format(Vo))
+print('Launch Angle: {0}\n'.format(angleD))
+print('Maximum Height: {0:.1f}\n'.format(maxH))
+print('Maximum Range: {0:.1f}'.format(maxR))
---
+++ JF/E4_P20_freidljs.py
@@ -0,0 +1,46 @@
+# Assignment
+# File:    E4_P20_freidljs
+# Date:    2/13/23
+# By:      Jacob Freidline
+# Section: 005
+# Team:    72
+# 
+# ELECTRONIC SIGNATURE
+# Jacob Freidline
+#
+# The electronic signature above indicates the script
+# submitted for evaluation is my individual work, and I
+# have a general understanding of all aspects of its
+# development and execution.
+#
+# A BRIEF DESCRIPTION OF WHAT THE SCRIPT OR FUNCTION DOES
+# This script is a header template that will be used for 
+# all your python files the rest of the semester.
+
+Data = open('Robot.txt','r')
+Results = open('E4_freidljs.txt','w')
+data = Data.readlines()
+
+counterB = 0
+counterX = 0
+counterY = 0
+
+for k in range(len(data)):
+    Vals = data[k].split()
+    if Vals[0] >= 19.125 and Vals[0] <= 19.5 and Vals[1] >= 58 and Vals[1] <= 59:
+       counterB = counterB+1 
+    else:  
+        if Vals[0] >= 19.125 and Vals[0] <= 19.5:
+            counterX = counterX+1
+        if Vals[1] >= 58 and Vals[1] <= 59:
+            counterY = counterY+1
+    Xerror = Vals[0]-19.3125
+    Yerror = Vals[1]-59
+    Results.write('{0:.4f}\t{1:.4f}\n'.format(Xerror,Yerror))
+
+print('Both x and y in acceptable range: {0}\n'.format(counterB))
+print('x in acceptable range: {0}\n'.format(counterX))
+print('y in acceptable range: {0}'.format(counterY))
+
+Data.close()
+Results.close()
